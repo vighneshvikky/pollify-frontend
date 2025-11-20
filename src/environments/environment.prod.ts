3c61c8b1079305f42d0d62e520149eedefad5627
Vighnesh SL <vighnesh55642@gmail.com>
2025-11-20 10:26:08 +0530

    chore: ignore environments folder

--- src/environments/environment.prod.ts
+++
@@ -1,7 +0,0 @@
-export const environment = {
-  production: true,
-  apiUrl: 'https://api.vortex-fit.space',
-  socketUrl: 'https://api.vortex-fit.space',
-  googleClientId: 'your-prod-google-client-id',
-
-};
